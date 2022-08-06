a3decf4ec933df953aa89660f1d4b3f23f4d705e
Rodrigo <rodrigo201101@hotmail.com>
2022-08-06 17:00:18 -0300

    template

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './styles/App.css'
+import Header from './components/Header'
+import MainVP from './components/MainVP'
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+      <MainVP />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../styles/header.css'
+
+export default function Header(){
+    return(
+        <header className="topVP">
+            <h1>Gintama</h1>
+            <img src="./src/img/logo.png" alt="SHIMATAAAAAA" />
+            <h1>Cards</h1>
+        </header>
+    )
+}
